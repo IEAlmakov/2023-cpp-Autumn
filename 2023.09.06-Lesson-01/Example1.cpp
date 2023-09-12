f6aa94c3cde6d20430bfca973bcf6615542cea9a
IEAlmakov <144109378+IEAlmakov@users.noreply.github.com>
2023-09-12 10:10:34 +0000

    modified:   .gitignore renamed:    2023.09.06-Lesson-01/Example_1.cpp -> 2023.09.06-Lesson-01/Example1.cpp new file:   2023.09.06-Lesson-01/Example2.cpp deleted:    2023.09.06-Lesson-01/Example_1

--- 2023.09.06-Lesson-01/Example1.cpp
+++ 2023.09.06-Lesson-01/Example1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    int a = 0, b = 0;
+    int a = 0;
+    int b = 0;
 	std::cin >> a >> b;
     std::cout << a + b;
     return 0;
---
+++ 2023.09.06-Lesson-01/Example2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+int our_sum(int an) {
+    int d = (abs(an - 1))/(an - 1);
+    int n = 1 + ((an - 1)/ d);
+    int s = (1 + an) * n / 2;
+	return s;
+}
+
+int main() {
+    int a_n = 0;
+	std::cin >> a_n;
+    a_n = our_sum(a_n);
+    std::cout << a_n;
+    return 0;
+}
