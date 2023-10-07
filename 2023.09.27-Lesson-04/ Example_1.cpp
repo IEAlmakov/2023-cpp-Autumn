ebfd44ab037c247d6f6d9d9c5df26c3cf5ec31eb
IEAlmakov <st117171@student.spbu.ru>
2023-10-08 00:31:57 +0300

    new file:   2023.09.27-Lesson-04/ Example_1.cpp

---
+++ 2023.09.27-Lesson-04/ Example_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) 
+{
+    int* array  = NULL;
+    int size    = 0;
+    int count   = 0;
+
+    std::cout << "Введите пожалуйста размер массива: ";
+    std::cin >> size;
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
