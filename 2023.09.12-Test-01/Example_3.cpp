c7967640e8bc53706f085462d4561d0dd6d7b3a3
IEAlmakov <st117171@student.spbu.ru>
2023-10-12 23:06:43 +0300

    modified:   2023.09.12-Test-01/Example_3.cpp

--- 2023.09.12-Test-01/Example_3.cpp
+++ 2023.09.12-Test-01/Example_3.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
-#include <cmath>
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
     int n = 0;
     std::cin >> n;
-
-    if (n >= 0) 
-    {
-        for (int i = 0; i <= n; ++i) 
-        {
-            std::cout << i << " ";
-        }
-    } 
-    else 
-    {
-        for (int i = 0; i >= n; --i) 
-        {
-            std::cout << i << " ";
-        }
-    }
     
-    for (int i = std::abs(n) - 1; i >= 0; --i) 
+    while (n > 0) 
     {
-        std::cout << ((n > 0) ? i : -i) << " ";
+        std::cout << n % 10;
+        n /= 10;
     }
-
     std::cout << std::endl;
+    
     return EXIT_SUCCESS;
 }
