0f09585488e779e0ea31698fb378d82b17d645ae
IEAlmakov <st117171@student.spbu.ru>
2023-10-26 17:49:28 +0300

    change menu

--- 2023.10.26-Lesson-05/Array_program/main.cpp
+++ 2023.10.26-Lesson-05/Array_program/main.cpp
@@ -24,42 +24,42 @@ int main(int argc, char* argv[])
 
         switch (option)
         {
-        case 1:
-            exit = true;
-            break;
-        case 2:
-            arrayPrint (array, len);
-            break;
-        case 3:
-            index = indexCheck(index);
-            element = elementCheck(element);
-            arrayAdd(&array, &len, index, element);
-            break;
-        case 4:
-            index = indexCheck(index);
-            arrayDelete(&array, &len, index);
-            break;
-        case 5:
-            arrayBinarySort(&array, len);
-            break;
-        case 6:
-            arrayReverse(&array, len);
-            break;
-        case 7:
-            arraySwitch(&array, len);
-            break;
-        case 8:
-            arrayDeleteDuplicate(&array, &len);
-            break;
-        case 9:
-            std::cout << "Введите пожалуйста размер массива" << std::endl;
-            correct(&len);
-            arrayCreate(&array, len);
-            arrayInitialization (&array, &len);
-            break;
-        default:
-            std::cout << "Введите пожалуйста другую опцию" << std::endl;
-            break;
+            case 1:
+                exit = true;
+                break;
+            case 2:
+                arrayPrint (array, len);
+                break;
+            case 3:
+                index = indexCheck(index);
+                element = elementCheck(element);
+                arrayAdd(&array, &len, index, element);
+                break;
+            case 4:
+                index = indexCheck(index);
+                arrayDelete(&array, &len, index);
+                break;
+            case 5:
+                arrayBinarySort(&array, len);
+                break;
+            case 6:
+                arrayReverse(&array, len);
+                break;
+            case 7:
+                arraySwitch(&array, len);
+                break;
+            case 8:
+                arrayDeleteDuplicate(&array, &len);
+                break;
+            case 9:
+                std::cout << "Введите пожалуйста размер массива" << std::endl;
+                correct(&len);
+                arrayCreate(&array, len);
+                arrayInitialization (&array, &len);
+                break;
+            default:
+                std::cout << "Введите пожалуйста другую опцию" << std::endl;
+                break;
         }
         std::cout << "Для продолжения нажмите любую клавишу" << std::endl;
         system("read");
