6a64908f10ce130827f61b2ed53313930ed41efe
st117173 <st117173@student.spbu.ru>
2024-04-19 01:55:23 +0300

    fix

--- 2024_04_18-Lesson-05/main.cpp
+++ 2024_04_18-Lesson-05/main.cpp
@@ -4,6 +4,7 @@ struct Node
 {
     int data;
     Node *next;
+
     Node() : data(0), next(nullptr)
     {
     }
@@ -13,10 +14,10 @@ struct Node
     ~Node()
     {
     }
+
     friend std::ostream &operator<<(std::ostream &stream, Node &node)
     {
-
-        std::cout << node.data;
+        stream << node.data;
         return stream;
     }
 };
@@ -28,39 +29,46 @@ class LinkedList {
     }
     ~LinkedList()
     {
-        dispose();
+        while (head != nullptr)
+        {
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+        }
     }
-    int Length()
+
+    int Length() const
     {
         int count = 0;
-        Node *node = head;
-        while (node != nullptr)
+        Node *current = head;
+        while (current != nullptr)
         {
-            count++;
-            node = node->next;
+            ++count;
+            current = current->next;
         }
         return count;
     }
 
-    bool IsEmpty()
+    bool IsEmpty() const
     {
         return head == nullptr;
-    };
+    }
 
     void PushHead(int data)
     {
         Node *node = new Node(data);
         InsertNode(0, node);
     }
+
     void Insert(int index, int data)
     {
         Node *node = new Node(data);
         InsertNode(index, node);
     }
+
     void PushTail(int data)
     {
-        Node *node = new Node(data);
-        InsertNode(Length(), node);
+        InsertNode(Length(), new Node(data));
     }
 
     int PopHead()
@@ -71,57 +79,48 @@ class LinkedList {
     int Extract(int index)
     {
         return PopData(ExtractNode(index));
-    };
+    }
+
     int PopTail()
     {
         return PopData(ExtractNode(Length() - 1));
     }
 
-    int Data(int index)
+    int Data(int index) const
     {
-        if (!IndexValid(index) || (Length() - 1 < index))
-        {
-            return 0;
-        }
-
         Node *current = head;
-        while (index > 0)
+        for (int i = 0; i < index && current != nullptr; ++i)
         {
             current = current->next;
-            index = index - 1;
         }
-        return current->data;
+        return current != nullptr ? current->data : 0;
     }
+
     void swap(int ia, int ib)
     {
-        if (!IndexValid(ia) || !IndexValid(ib) || ia == ib)
+        if (ia == ib || !IndexValid(ia) || !IndexValid(ib))
         {
             return;
         }
-
         if (ia > ib)
         {
-            int tmp = ib;
-            ib = ia;
-            ia = tmp;
+            std::swap(ia, ib);
         }
-        Node *nodeB = ExtractNode(ib);
         Node *nodeA = ExtractNode(ia);
-
+        Node *nodeB = ExtractNode(ib - 1);
         if (nodeA == nullptr || nodeB == nullptr)
         {
             return;
         }
-
         InsertNode(ia, nodeB);
         InsertNode(ib, nodeA);
     }
 
     void sort()
     {
-        for (int i = Length() - 1; i >= 0; i--)
+        for (int i = Length() - 1; i >= 0; --i)
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 0; j < i; ++j)
             {
                 if (Data(j) > Data(j + 1))
                 {
@@ -131,7 +130,7 @@ class LinkedList {
         }
     }
 
-    friend std::ostream &operator<<(std::ostream &stream, LinkedList &list)
+    friend std::ostream &operator<<(std::ostream &stream, const LinkedList &list)
     {
         Node *current = list.head;
         while (current != nullptr)
@@ -140,61 +139,21 @@ class LinkedList {
             current = current->next;
         }
         return stream;
-    };
+    }
 
     private:
-    // Как рыба гниет с головы, так и список тоже очищаем с головы
-    //(C) защищено авторским правом бай альмаков илья евгеньевич
-    void dispose()
+    bool IndexValid(int index) const
     {
-        while (head != nullptr)
+        if (index < 0)
         {
-            Node *temp = head;
-            head = head->next;
-            delete temp;
+            return false;
         }
-    }
-
-    int PopData(Node *node)
-    {
-        if (node == nullptr)
+        Node *current = head;
+        for (int i = 0; i < index && current != nullptr; ++i)
         {
-            return 0;
+            current = current->next;
         }
-        int res = node->data;
-        delete node;
-        return res;
-    }
-    void InsertNode(int index, Node *node)
-    {
-        if (!IndexValid(index))
-        {
-            return;
-        }
-        if (index == 0)
-        {
-            node->next = head;
-            head = node;
-        }
-        else if (index == Length())
-        {
-            Node *current = head;
-            while (current->next != nullptr)
-            {
-                current = current->next;
-            }
-            current->next = node;
-        }
-        else if (index < Length())
-        {
-            Node *current = head;
-            for (int i = 0; i < index - 1; i++)
-            {
-                current = current->next;
-            }
-            node->next = current->next;
-            current->next = node;
-        }
+        return current != nullptr;
     }
 
     Node *ExtractNode(int index)
@@ -209,42 +168,49 @@ class LinkedList {
             head = head->next;
             return temp;
         }
-        if (index == Length() - 1)
+        Node *current = head;
+        for (int i = 0; i < index - 1; ++i)
         {
-            Node *current = head;
-            while (current->next->next != nullptr)
-            {
-                current = current->next;
-            }
-            Node *temp = current->next;
-            current->next = nullptr;
-            return temp;
+            current = current->next;
         }
-        if (index < Length())
-        {
-            Node *current = head;
-            for (int i = 0; i < index - 1; i++)
-            {
-                current = current->next;
-            }
-            Node *temp = current->next;
-            current->next = current->next->next;
-            return temp;
-        }
-        return nullptr;
+        Node *temp = current->next;
+        current->next = temp->next;
+        return temp;
     }
-    bool IndexValid(int index)
+
+    void InsertNode(int index, Node *node)
     {
-        if (index < 0 || index > Length())
+        if (index <= 0 || head == nullptr)
         {
-            return false;
+            node->next = head;
+            head = node;
         }
-        return true;
+        else
+        {
+            Node *current = head;
+            for (int i = 0; i < index - 1 && current->next != nullptr; ++i)
+            {
+                current = current->next;
+            }
+            node->next = current->next;
+            current->next = node;
+        }
+    }
+
+    int PopData(Node *node)
+    {
+        if (node == nullptr)
+        {
+            return 0;
+        }
+        int res = node->data;
+        delete node;
+        return res;
     }
     Node *head;
 };
 
-int main(int argc, char *argv[])
+int main()
 {
     LinkedList list;
     std::cout << list << std::endl;
@@ -271,8 +237,9 @@ int main(int argc, char *argv[])
     list.PushHead(5);
     list.PushHead(8);
     list.PushHead(6);
+
     std::cout << list << std::endl;
     list.sort();
     std::cout << list << std::endl;
-    return EXIT_SUCCESS;
+    return 0;
 }
